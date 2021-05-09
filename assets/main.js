60924d45a5d97b78c215d7ebd88ca7286da77689
phoebe <phoebe@phoebe2019.local>
2021-05-08 20:24:31 -0400

    organized files, commented code

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,12 @@
 /* view background gif when hovering over 'check out my work' */
 
+/* gif appears upon hovering over text */
 $('.portfolio').mouseover(function() {
     b = document.getElementById("bg-gif");
     b.classList.remove("hidden");
 });
 
+/* gif disappears after hovering away from text */
 $('.portfolio').mouseout(function() {
     b = document.getElementById("bg-gif");
     b.classList.add("hidden");
